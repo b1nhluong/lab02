a55ad54eca053c8c3a1646de3022c234cafa29b7
Luong Thanh Binh <159543438+b1nhluong@users.noreply.github.com>
2024-10-24 22:36:01 +0700

    Add files via upload

---
+++ AimsProject/src/Aims.java
@@ -0,0 +1,19 @@
+
+public class Aims {
+	public static void main(String[] args) {
+		Cart cart = new Cart();
+
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+		cart.addDigitalVideoDisc(dvd1);
+		cart.addDigitalVideoDisc(dvd2);
+		cart.addDigitalVideoDisc(dvd3);
+
+		System.out.print("The total cost is: ");
+		System.out.println(cart.totalCost());
+
+		cart.removeDigitalVideoDisc(dvd3);
+	}
+}
