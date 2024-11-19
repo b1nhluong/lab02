5405299dc2434073022cf592e1ccc0a49cec76d8
Luong Thanh Binh <159543438+b1nhluong@users.noreply.github.com>
2024-11-19 16:06:43 +0700

    Update Aims.java
    Add a new topic

--- AimsProject/src/Aims.java
+++ AimsProject/src/Aims.java
@@ -7,11 +7,16 @@ public class Aims {
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 
+		
+		
+		cart.addDigitalVideoDisc(dvd1);
+		cart.addDigitalVideoDisc(dvd2);
+		cart.addDigitalVideoDisc(dvd3);
 		cart.addDigitalVideoDisc(new DigitalVideoDisc[] {dvd1, dvd2});
-
+		cart.addDigitalVideoDisc(dvd2, dvd3);
 		System.out.print("The total cost is: ");
 		System.out.println(cart.totalCost());
 
-		cart.removeDigitalVideoDisc(dvd2);
+		cart.removeDigitalVideoDisc(dvd3);
 	}
 }
