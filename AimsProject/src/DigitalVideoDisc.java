af5f5722958e0eb776a98bf7e845007469c05607
Luong Thanh Binh <159543438+b1nhluong@users.noreply.github.com>
2024-11-22 10:50:02 +0700

    Update DigitalVideoDisc.java
    update

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -1,58 +1,74 @@
-
 public class DigitalVideoDisc {
-	private String title;
-	private String category;
-	private String director;
-	private int length;
-	private float cost;
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
+    
+    public String getTitle() {
+        return title;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getCategory() {
-		return category;
-	}
+    public String getCategory() {
+        return category;
+    }
 
-	public String getDirector() {
-		return director;
-	}
+    public String getDirector() {
+        return director;
+    }
 
-	public int getLength() {
-		return length;
-	}
+    public int getLength() {
+        return length;
+    }
 
-	public float getCost() {
-		return cost;
-	}
+    public float getCost() {
+        return cost;
+    }
 
-	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
-	}
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
 
-	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-	}
+    public int getId() {
+        return id;
+    }
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        assignId();
+    }
 
-	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.cost = cost;
-	}
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        assignId();
+    }
 
-	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.length = length;
-		this.cost = cost;
-	}
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+        assignId();
+    }
 
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        assignId();
+    }
+
+    private void assignId() {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
 }
